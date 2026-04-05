099dbd049493761073a285690f16de215237225e
Murilo Unten <murilo.unten@proton.me>
2026-04-04 22:54:48 -0300

    wip: logging service

--- common/globals.go
+++ common/globals.go
@@ -2,8 +2,14 @@ package common
 
 const (
 	Url = "amqp://guest:guest@localhost:5672/assignment1"
-	ExchangeName = "promocoes"
 
+	ExchangeName = "promocoes"
 	ReceivedKey = "promocao.recebida"
 	PublishedKey = "promocao.publicada"
+	
+	LogsExchangeName = "logs"
+	InfoKey = "info"
+	WarningKey = "warn"
+	ErrorKey = "error"
+	DebugKey = "debug"
 )
---
+++ logging/go.mod
@@ -0,0 +1,10 @@
+module logging
+
+go 1.26.1
+
+require (
+	github.com/MuriloUnten/distributed-sales/common v0.0.0-20260404215614-ba2dbf87b179
+	github.com/rabbitmq/amqp091-go v1.10.0
+)
+
+replace github.com/MuriloUnten/distributed-sales/common => ../common
